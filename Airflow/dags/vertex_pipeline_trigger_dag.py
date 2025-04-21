a56d201239733a6b0e33a00d978d3ea517cb25a1
navya-24-01 <navyamunjal2401@gmail.com>
2025-04-21 19:06:31 +0800

    vertex pipeline dag

--- Airflow/dags/vertex_pipeline_trigger_dag.py
+++ Airflow/dags/vertex_pipeline_trigger_dag.py
@@ -1,26 +1,26 @@
 from airflow import models
-from airflow.providers.google.cloud.operators.vertex_ai import CreatePipelineJobOperator
+from airflow.providers.google.cloud.operators.vertex_ai.pipeline_job import (
+    RunPipelineJobOperator,
+)
 from datetime import datetime
 
 PROJECT_ID = "is3107-453814"
 REGION = "us-central1"
-PIPELINE_NAME = "car-price-retrain-pipeline"
-PIPELINE_ROOT = "gs://is3107-car-data/pipeline-artifacts"
-TEMPLATE_PATH = f"{PIPELINE_ROOT}/retrain_pipeline.json"
+TEMPLATE_PATH = "gs://is3107-car-data/pipeline-artifacts/retrain_pipeline.json"
 
 with models.DAG(
     dag_id="trigger_vertex_pipeline_dag",
-    schedule_interval=None,  # or '0 9 * * *' for daily at 9 AM
+    schedule_interval=None,
     start_date=datetime(2024, 1, 1),
     catchup=False,
     tags=["vertex", "car-price"],
 ) as dag:
 
-    trigger_pipeline = CreatePipelineJobOperator(
+    run_pipeline_job = RunPipelineJobOperator(
         task_id="run_vertex_pipeline",
-        project_id=PROJECT_ID,
-        region=REGION,
         display_name="trigger-car-price-pipeline",
         template_path=TEMPLATE_PATH,
-        pipeline_root=PIPELINE_ROOT,
+        region=REGION,
+        project_id=PROJECT_ID,
+        service_account="airflow-service-account@is3107-453814.iam.gserviceaccount.com",
     )
