309113e821003ed4e3f0d1926d6c06bc441f5909
navya-24-01 <navyamunjal2401@gmail.com>
2025-04-27 17:06:28 +0530

    trigger waits for prev dag

--- Airflow/dags/vertex_pipeline_trigger_dag.py
+++ Airflow/dags/vertex_pipeline_trigger_dag.py
@@ -2,11 +2,11 @@ from airflow import models
 from airflow.providers.google.cloud.operators.vertex_ai.pipeline_job import (
     RunPipelineJobOperator,
 )
-from datetime import datetime
+from airflow.sensors.external_task import ExternalTaskSensor
+from datetime import datetime, timedelta
 
 PROJECT_ID = "is3107-453814"
 REGION = "us-central1"
-# TEMPLATE_PATH = "gs://is3107-car-data/pipeline-artifacts/retrain_pipeline.json"
 TEMPLATE_PATH = "gs://is3107-bucket/mlops/retrain_pipeline.json"
 
 with models.DAG(
@@ -17,6 +17,18 @@ with models.DAG(
     tags=["vertex", "car-price"],
 ) as dag:
 
+    wait_for_etl = ExternalTaskSensor(
+        task_id="wait_for_sgcarmart_etl",   
+        external_dag_id="SGCarMart_ETL_DAG",            
+        allowed_states=["success"],
+        failed_states=["failed"],
+        timeout=60 * 60,  
+        poke_interval=30,  
+        mode="poke",
+        dag=dag,
+    )
+
+
     run_pipeline_job = RunPipelineJobOperator(
         task_id="run_vertex_pipeline",
         display_name="trigger-car-price-pipeline",
@@ -25,3 +37,5 @@ with models.DAG(
         project_id=PROJECT_ID,
         service_account="airflow-service-account@is3107-453814.iam.gserviceaccount.com",
     )
+
+    wait_for_etl >> run_pipeline_job
